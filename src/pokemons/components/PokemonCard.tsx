dcc2dfc15bf6a92ba44b9a3c205b70686dd08ed2
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-05-20 11:31:19 -0700

    Se agrega busqueda por name

--- src/pokemons/components/PokemonCard.tsx
+++ src/pokemons/components/PokemonCard.tsx
@@ -25,13 +25,13 @@ export const PokemonCard = ({pokemon}: Props) => {
                     />
                     <p className="pt-2 text-lg font-semibold text-gray-50 capitalize">{name}</p>
                     <div className="mt-5">
-                        <Link href="#" className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100">
+                        <Link href={`/dashboard/pokemons/${name}`} className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100">
                             Más información
                         </Link>
                     </div>
                 </div>
                 <div className="border-b">
-                    <Link href={`/dashboard/pokemon/${id}`} className="px-4 py-2 hover:bg-gray-100 flex items-center">
+                    <Link href='/dashboard/main/' className="px-4 py-2 hover:bg-gray-100 flex items-center">
                             <div className="text-red-600">
                                 <IoHeartCircleOutline />
                             </div>
