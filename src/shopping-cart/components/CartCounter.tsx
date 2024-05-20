a854a9c21a1ff401942785166f8a9a1672074103
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-05-20 14:55:01 -0700

    Global State- Redux y LocalStorage

--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -1,14 +1,23 @@
 
 'use client';
 
-import { useState } from "react";
+import { useAppSelector } from "@/store";
+import { addOne, initCounterState, substractOne } from "@/store/counter/counter-slice";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 interface Props{
   value? : number;
 }
 
 export const CartCounter = ({value = 0}: Props) => {
-  const [count, setCount] = useState(value);
+
+  const count = useAppSelector(state => state.counter.count);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(initCounterState(value));
+  }, [dispatch, value]);
 
   return (
     <>
@@ -16,13 +25,13 @@ export const CartCounter = ({value = 0}: Props) => {
       <div className="flex">
         
       <button
-          onClick={() => setCount(count - 1)}
+          onClick={() => dispatch(substractOne())}
          className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
           -1
         </button>
 
         <button 
-        onClick={() => setCount(count + 1)}
+         onClick={() => dispatch(addOne())}
         className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2">
           +1
         </button>
