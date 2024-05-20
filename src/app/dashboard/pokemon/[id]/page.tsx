519e41357d8789cf5921500c18abba2c8fc323a2
Felix Javier Nunez Gonzalez <nem.fnunez@coppel.com>
2024-05-20 10:03:15 -0700

    Generacion estatica

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -7,6 +7,18 @@ interface Props{
   params: {id: string};
 }
 
+//!En build time
+export async function generateStaticParams(){
+  return [
+    {id: '1'},
+    {id: '2'},
+    {id: '3'},
+    {id: '4'},
+    {id: '5'},
+    {id: '6'},
+  ]
+}
+
 export async function generateMetadata({params}: Props) : Promise<Metadata>{
 
   try {
